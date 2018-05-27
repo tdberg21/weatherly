f74877eff1e8da66f9d7879f9cb2054c667f9efe
Tory Dannenberg <rfdberg@gmail.com>
2018-05-27 08:46:36 -0600

    Fix ten day file

--- src/components/TenDay/TenDay.js
+++ src/components/TenDay/TenDay.js
@@ -2,27 +2,10 @@ import React, { Component } from 'react';
 import Card from '../Card/Card.js'
 import cleanTenData from '../../dataCleaner/cleanTenData.js'
 
-console.log(cleanTenData())
-
 class TenDay extends Component {
   constructor() {
     super();
   };
-
-  render() {
-    return (
-      
-      <div>
-      <Card 
-        dayName = {cleanTenData()[1].day}
-        image= {cleanTenData()[1].img}
-        high = {cleanTenData()[1].high}
-        low = {cleanTenData()[1].low}
-      />
-      </div>
-      )
-  }
-}
 
   render() {
     return (
@@ -37,4 +20,6 @@ class TenDay extends Component {
       })}
       </div>
     )
-}};
+}}
+
+export default TenDay;
